b232c22d46eaf8cb0c2faf44c2707aa72dd48222
Fernando Mendes <devfrmendes@gmail.com>
2016-09-23 17:24:21 +0100

    Manually deploying halfway through the conference. #yolo

---
+++ javascripts/nav-bbeb81bd.js
@@ -0,0 +1,1 @@
+window.Nav={listenToBurger:function(){$("#Burger, #Overlay a").on("click",Nav.toggleNav)},toggleNav:function(){$("#Overlay, #Burger").toggleClass("is-open"),$("body").toggleClass("no-scroll")}};
